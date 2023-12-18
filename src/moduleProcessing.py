16217f2e0349ab7f17776918f197183e34713dfe
HiddenVault <144142448+HiddenVault@users.noreply.github.com>
2023-12-18 14:27:59 +0100

    moduleProcessing.py : Ajout des fonctions create_column, rounded_column, drop_duplicates, change_column_type, fillna_column & Cosmétiques
    processing.py : Ajout des nouvelles fonctions dans le script

--- src/moduleProcessing.py
+++ src/moduleProcessing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import math
 import json
 import warnings
 
@@ -7,38 +8,35 @@ Explications :
 La fonction create_column_id prend en entrée :
     - df : Nom du dataframe utilisé
     - prefix : Préfixe utilisé précédant la date
-    - origin_column : Nom de la colonne ou se trouve la date
+    - original_column : Nom de la colonne ou se trouve la date
     - target_column : Nom de la colonne à créer
 
 Etapes du script :
-    - Conversion de la colonne origin_column en format date
-    - Extraction de la date dans origin_column et concaténation avec prefix pour la création de l'identifiant
+    - Conversion de la colonne original_column en format date
+    - Extraction de la date dans original_column et concaténation avec prefix pour la création de l'identifiant
     - Enregistrement de l'identifiant dans la colonne target_column
     - Déplacement de la nouvelle au début du dataframe
 
-Exemple : create_column_id(df, prefix= 'degradations', origin_column = 'measure_day', target_column = 'id')
+Exemple : create_column_id(df, prefix= 'degradations', original_column = 'measure_day', target_column = 'id')
 '''
-
-
-def create_column_id(df, prefix, origin_column, target_column): 
+def create_column_id(df, prefix, original_column, target_column): 
     print()
-    print(f"\n💾 Traitement du dataframe en cours")
-    print(df.head(5))
+    print(f"🛫 Création de la colonne en cours", end='')
+    # print(df.head(5))
     # Conversion de la colonne en format datetime
-    df[origin_column] = pd.to_datetime(df[origin_column], errors='coerce')
-    print(f"\n✅ Conversion de la colonne en format Date/Time", end='')
+    df[original_column] = pd.to_datetime(df[original_column], errors='coerce')
+    print(f"\n✅ Conversion de la colonne {original_column} en format Date/Time", end='')
 
     # Extraction de la date au format YYYY-MM-DD et concaténation avec le préfixe
-    df[target_column] = prefix + '_' + df[origin_column].dt.strftime('%Y-%m-%d')
+    df[target_column] = prefix + '_' + df[original_column].dt.strftime('%Y-%m-%d')
     print(f"\n✅ Concaténation de la date et du préfixe", end='')
 
     # Déplacement de target_column en première position
     target_column_series = df.pop(target_column)
     df.insert(0, target_column, target_column_series)
-    print(f"\n✅ Réorganisaton des colonnes")
-
-    print(f"\n💾 Traitement du dataframe terminé")
-    print(df.head(5))
+    print(f"\n✅ Réorganisaton des colonnes", end='')
+    print(f"\n🛬 Création terminée")
+    # print(df.head(5))
 
     return df
 
@@ -49,7 +47,7 @@ La fonction split_column_json prend en entrée :
     - column_name : Nom de la colonne à diviser
 
 Etapes du script :
-    - Vérification de l'existance de la colonne
+    - Vérification de l'existence de la colonne
     - Division de la colonne "sensor_data" en colonnes distinctes avec pd.json_normalize().
 
 Exemple : split_column_json(df, column_name = 'sensor_data')
@@ -57,12 +55,12 @@ Exemple : split_column_json(df, column_name = 'sensor_data')
 def split_column_json(df, column_name):
     warnings.simplefilter('ignore')
     
-    # Vérification de l'existance de la colonne
+    # Vérification de l'existence de la colonne
     if column_name not in df.columns:
-        print(f"❌ La colonne '{column_name}' n'existe pas.")
+        print(f"\n❌ La colonne '{column_name}' n'existe pas.", end='')
         return df
-    print(f"\n💾 Traitement du dataframe en cours")
-    print(df.head(5))
+    print(f"\n🛫 Division de la colonne {column_name} en cours", end='')
+    # print(df.head(5))
 
     def extract_sensor_data(sensor_data):
         try:
@@ -76,7 +74,7 @@ def split_column_json(df, column_name):
             vibrations = float(data.get('vibrations').replace('m/s²', ''))
             return temp, pressure, vibrations
         except Exception as e:
-            print(f"❌ Erreur lors de l'extraction des données : {e}")
+            print(f"\n❌ Erreur lors de l'extraction des données : {e}", end='')
             # Autant de none que de colonnes
             return None, None, None
 
@@ -90,11 +88,11 @@ def split_column_json(df, column_name):
     print(f"\n✅ Division de sensor_data en colonnes distinctes", end='')
 
     # Suppression de la colonne
-    print(f"\n✅ Suppression de la colonne sensor_data")
+    print(f"\n✅ Suppression de la colonne sensor_data", end='')
     df = df.drop([column_name], axis=1)
 
-    print(f"\n💾 Traitement du dataframe terminé")
-    print(df.head(5))
+    print(f"\n🛬 Division terminée")
+    # print(df.head(5))
 
     return df
 
@@ -111,10 +109,9 @@ Etapes du script :
 
 Exemple : create_column(df, created_column= 'img', position_column = 2, default_value = 'none.png')
 '''
-
 def create_column(df, created_column, position_column, default_value):
-    print(f"\n💾 Traitement du dataframe en cours")
-    print(df.head(5))
+    print(f"🛫 Création de la colonne en cours", end='')
+    # print(df.head(5))
 
     # Vérification de la validité de la position
     if position_column < 0 or position_column > len(df.columns):
@@ -138,9 +135,171 @@ def create_column(df, created_column, position_column, default_value):
     columns.pop()
     columns.insert(position_column, created_column)
     df = df[columns]
-    print(f"\n✅ Réorganisaton des colonnes")
+    print(f"\n✅ Réorganisaton des colonnes", end='')
 
-    print(f"\n💾 Traitement du dataframe terminé")
-    print(df.head(5))
+    print(f"\n🛬 Création terminée")
+    # print(df.head(5))
+
+    return df
+
+'''
+Explications :
+La fonction create_column prend en entrée :
+    - df : Nom du dataframe utilisé
+    - original_column : Nom de la colonne à renommer.
+    - renamed_column : Nouveau nom
+
+Etapes du script :
+    - Renommage de la colonne 
+
+Exemple : rename_column(df, original_column= 'aero_linked', renamed_column = 'ref_aero')
+'''
+def rename_column(df, original_column, renamed_column):
+    print(f"\n🛫 Renommage de la colonne en cours", end='')
+    # print(df.head(5))
+
+    # Vérification de l'existence de la colonne
+    if original_column not in df.columns:
+        raise ValueError(f"❌ La colonne '{original_column}' n'existe pas.")
+
+    # Renommage de la colonne
+    df.rename(columns={original_column: renamed_column}, inplace=True)
+
+    print(f"\n✅ Renommage de la colonne", end='')
+
+    print(f"\n🛬 Renommage terminé")
+    # print(df.head(5))
+
+    return df
+
+'''
+Explications :
+La fonction rounded_column prend en entrée :
+    - df : Nom du dataframe utilisé
+    - original_column : Nom de la colonne à arrondir.
+    - rounded_value : Choix de l'arrondi
+
+Etapes du script :
+    - Renommage de la colonne 
+
+Exemple : rounded_column(df, original_column= 'taux_usure_actuel', rounded_value = 'sup')
+'''
+def rounded_column(df, original_column, rounded_value):
+    print(f"\n🛫 Arrondissement des valeurs numériques en cours")
+    # print(df.head(5))
+
+    # Vérification de l'argument 'arrondi'
+    if rounded_value not in ['inf', 'sup']:
+        raise ValueError("❌ L'argument 'rounded_value' doit être 'inf' ou 'sup'.")
+
+    # Vérification de l'existence de la colonne
+    if original_column not in df.columns:
+        raise ValueError(f"❌ La colonne '{original_column}' n'existe pas.")
+
+    # Application de l'arrondi à la colonne
+    if rounded_value == 'inf':
+        df[original_column] = df[original_column].apply(lambda x: math.floor(x * 100) / 100)
+        print(f"✅ Valeur arrondie à l'inférieur", end='')
+    elif rounded_value == 'sup':
+        df[original_column] = df[original_column].apply(lambda x: math.ceil(x * 100) / 100)
+        print(f"✅ Valeur arrondie au supérieur", end='')
+    
+    print(f"\n🛬 Arrondissement terminé")
+    # print(df.head(5))
+    return df
+
+'''
+Explications :
+La fonction drop_duplicates prend en entrée :
+    - df : Nom du dataframe utilisé
+    - original_column : Nom de la colonne à traiter.
+
+Etapes du script :
+    - Suppression des lignes dupliquées dans une colonne du dataframe 
+
+Exemple : drop_duplicates(df, original_column = 'compo_concerned', inplace = 'true')
+'''
+def drop_duplicates(df, original_column, inplace):
+    print(f"\n🛫 Suppression des lignes dupliquées en cours", end='')
+    print(f"✅ Nombre de lignes : {len(df)}")
+    # print(df.head(5))
+
+    # Vérification de l'existence de la colonne
+    if original_column not in df.columns:
+        raise ValueError(f"❌ La colonne '{original_column}' n'existe pas.")
+
+    # Suppression des lignes dupliquées
+    if inplace:
+        df.drop_duplicates(subset=[original_column], inplace=True)
+        print(f"\n✅ Suppression des lignes dupliquées avec inplace = {inplace}", end='')
+        print(f"\n🛬 Suppression terminée", end='')
+        print(f"\n✅ Nombre de lignes : {len(df)}")
+        # print(df.head(5))    
+        return df
+    else:
+        print(f"\n✅ Suppression des lignes dupliquées avec inplace = {inplace}", end='')
+        print(f"\n🛬 Suppression terminée", end='')
+        print(f"\n✅ Nombre de lignes : {len(df)}")
+        # print(df.head(5))  
+        return df.drop_duplicates(subset=[original_column])
+
+'''
+Explications :
+La fonction change_column_type prend en entrée :
+    - df : Nom du dataframe utilisé
+    - original_column : Nom de la colonne à traiter
+    - new_type : Nouveau type de la colonne
+
+Etapes du script :
+    - Changement du type d'une colonne par un autre 
+
+Exemple : change_column_type(df, original_column = 'compo_concerned', new_type = 'datetime64')
+'''
+def change_column_type(df, original_column, new_type):
+    print(f"\n🛫 Changement du type de la colonne en cours", end='')
+    # print(df.head(5))
+    
+    # Vérification de l'existence de la colonne
+    if original_column not in df.columns:
+        raise ValueError(f"❌ La colonne '{original_column}' n'existe pas.")
+
+    # Changement du type de la colonne
+    df[original_column] = df[original_column].astype(new_type)
+    print(f"\n✅ Changement du type {df[original_column].dtype} de la colonne {original_column} par {new_type}", end='')
+    print(f"\n🛬 Changement du type terminé")
+    # print(df.head(5)) 
+
+    return df
+
+'''
+Explications :
+La fonction fillna_column prend en entrée :
+    - df : Nom du dataframe utilisé
+    - original_column : Nom de la colonne à traiter
+    - type_NaN : type de valeur à utiliser pour les valeurs manquantes ('NaN' ou 'NaT')
+
+Etapes du script :
+    - Remplissage d'une colonne par  
+
+Exemple : fillna_column(df, original_column = 'compo_concerned', type_NaN = 'NaT')
+'''
+def fillna_column(df, original_column, type_NaN='NaN'):
+    print(f"\n🛫 Remplissage de colonne avec des valeurs {type_NaN}", end='')
+    # print(df.head(5))
+
+    # Vérification de l'existance de la colonne
+    if original_column not in df.columns:
+        raise ValueError(f"❌ La colonne '{original_column}' n'existe pas.")
+
+    # Remplir les valeurs manquantes en fonction du type spécifié
+    if type_NaN == 'NaN':
+        df[original_column].fillna('NaN', inplace=True)
+        print(f"\n✅ Remplissage de la colonne {original_column} avec {type_NaN}", end='')
+    elif type_NaN == 'NaT':
+        df[original_column].fillna(pd.NaT, inplace=True)
+        print(f"\n✅ Remplissage de la colonne {original_column} avec {type_NaN}", end='')
+    
+    print(f"\n🛬 Remplissage terminé")
+    # print(df.head(5))
 
     return df
--- src/processing.py
+++ src/processing.py
@@ -2,26 +2,42 @@ import moduleProcessing
 import pandas as pd
 
 # Traitement du dataset degradations_FULL
+print(f"\n🚀 Traitement du dataset degradations", end='')
 link = 'data/preprocessing/degradations_FULL.csv'
 df = pd.read_csv(link)
-
-df = moduleProcessing.create_column_id(df, 'degradations', 'measure_day', 'id')
+df = moduleProcessing.drop_duplicates(df, original_column = 'compo_concerned', inplace = 'true')
+df = moduleProcessing.create_column_id(df, prefix= 'degradations', original_column = 'measure_day', target_column = 'id')
+df = moduleProcessing.rename_column(df, original_column= 'linked_aero', renamed_column = 'ref_aero')
+df = moduleProcessing.rename_column(df, original_column= 'usure_cumulée', renamed_column = 'usure_cumulee')
+df = moduleProcessing.change_column_type(df, original_column = 'measure_day', new_type = 'datetime64[ns]')
+df = moduleProcessing.rounded_column(df, original_column= 'usure_cumulee', rounded_value = 'sup')
 df.to_csv('data/processing/degradations.csv', index=False)
 
 # Traitement du dataset logs_vols_FULL
+print(f"\n🚀 Traitement du dataset logs_vols", end='')
 link = 'data/preprocessing/logs_vols_FULL.csv'
 df = pd.read_csv(link)
 df = moduleProcessing.create_column_id(df, 'logs_vols', 'jour_vol', 'id')
 df = moduleProcessing.split_column_json(df, column_name = 'sensor_data')
+df = moduleProcessing.rename_column(df, original_column= 'aero_linked', renamed_column = 'ref_aero')
+df = moduleProcessing.change_column_type(df, original_column = 'jour_vol', new_type = 'datetime64[ns]')
 df.to_csv('data/processing/logs_vols.csv', index=False)
 
-# Traitement du dataset  
-# link = 'data/incoming/composants_2023-11-07.csv'
-# df = pd.read_csv(link)
+# Traitement du dataset composant
+print(f"\n🚀 Traitement du dataset composants", end='')
+link = 'data/incoming/composants_2023-11-07.csv'
+df = pd.read_csv(link)
+df = moduleProcessing.rename_column(df, original_column= 'aero', renamed_column = 'ref_aero')
+df = moduleProcessing.rounded_column(df, original_column= 'taux_usure_actuel', rounded_value = 'sup')
+df.to_csv('data/processing/composants.csv', index=False)
 
-
-# Traitement du dataset  
+# Traitement du dataset aeronefs
+print(f"\n🚀 Traitement du dataset aeronefs", end='')  
 link = 'data/incoming/aeronefs_2023-11-07.csv'
 df = pd.read_csv(link)
 df = moduleProcessing.create_column(df, created_column= 'img', position_column = 2, default_value = 'none.png')
+df = moduleProcessing.change_column_type(df, original_column = 'debut_service', new_type = 'datetime64[ns]')
+df = moduleProcessing.change_column_type(df, original_column = 'last_maint', new_type = 'datetime64[ns]')
+df = moduleProcessing.change_column_type(df, original_column = 'end_maint', new_type = 'datetime64[ns]')
+df = moduleProcessing.fillna_column(df, original_column = 'end_maint', type_NaN = 'NaT')
 df.to_csv('data/processing/aeronefs.csv', index=False)
